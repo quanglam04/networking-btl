0f4cea9cfdf5649435f8da876e004c7150570fb9
quanglam04 <trinhquanglam2k4@gmail.com>
2025-10-22 15:31:50 +0700

    feat: login

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express'
 import User from '~/models/User'
 import HTTPStatus from '~/shared/constants/httpStatus'
 import logger from '~/shared/utils/log'
+import * as jwt from 'jsonwebtoken'
 const test = (req: Request, res: Response) => {
   res.json({ message: 'OK' })
 }
 
 const register = async (req: Request, res: Response) => {
   logger.info('Đăng ký người dùng mới')
-
   try {
     const { username, password } = req.body
     const existingUser = await User.findOne({ username })
@@ -56,8 +56,56 @@ const register = async (req: Request, res: Response) => {
   }
 }
 
-const login = async (req: Request, res: Response) => {}
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body
+  logger.info(`User [${username}] đăng nhập vào hệ thống`)
+  if (!username || !password) {
+    return res.status(HTTPStatus.BAD_REQUEST).json({
+      status: HTTPStatus.BAD_REQUEST,
+      message: 'Vui lòng cung cấp đầy đủ tên đăng nhập và mật khẩu.'
+    })
+  }
+  try {
+    const user = await User.findOne({ username: username })
+    if (!user) {
+      return res.status(HTTPStatus.UNAUTHORIZED).json({
+        status: HTTPStatus.UNAUTHORIZED,
+        message: 'Tên đăng nhập hoặc mật khẩu không đúng.'
+      })
+    }
+    const isPasswordValid = password === user.password
+    if (!isPasswordValid) {
+      return res.status(HTTPStatus.UNAUTHORIZED).json({
+        status: HTTPStatus.UNAUTHORIZED,
+        message: 'Tên đăng nhập hoặc mật khẩu không đúng.'
+      })
+    }
+    const Payload = {
+      id: user.id,
+      username: user.username,
+      status: 'online',
+      lastSeen: user.lastSeen
+    }
+    const access_token = jwt.sign(Payload, process.env.JWT_SECRET as string, {
+      expiresIn: '1d'
+    })
+    return res.status(HTTPStatus.OK).json({
+      status: HTTPStatus.OK,
+      message: 'Đăng nhập thành công',
+      data: {
+        access_token: access_token,
+        user_info: Payload
+      }
+    })
+  } catch (error) {
+    console.error('Lỗi đăng nhập:', error)
+    return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+      status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      message: 'Đã xảy ra lỗi hệ thống.'
+    })
+  }
+}
 
-const logout = () => {}
+const logout = (req: Request, res: Response) => {}
 
 export { test, register, login, logout }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { login, logout, register, test } from '~/controllers/user.controller'
+import { authenticateToken } from '~/middlewares/user.middleware'
 
 export const userRouter = express.Router()
 
-userRouter.get('/test', test)
+userRouter.get('/test', authenticateToken, test)
 userRouter.post('/login', login)
 userRouter.post('/register', register)
 userRouter.post('/logout', logout)
