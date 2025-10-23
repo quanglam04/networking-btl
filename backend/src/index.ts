c351169d82af9e70072d3f74e9c210d9acd942bc
quanglam04 <trinhquanglam2k4@gmail.com>
2025-10-23 15:52:53 +0700

    tích hợp luồng login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,16 @@ import { connectDB } from './config/db.config'
 import './models/User'
 import './models/Message'
 import './models/Conversation'
+import cors from 'cors'
 import { userRouter } from './routes/user.routes'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+app.use(
+  cors({
+    origin: process.env.ALLOW_ORIGIN
+  })
+)
 
 app.use(express.json())
 app.use('/api/user', userRouter)
